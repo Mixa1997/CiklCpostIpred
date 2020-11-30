3ec5e013f34943c4844bf39e544f456cd306e2c5
Mixa1997 <43315299+Mixa1997@users.noreply.github.com>
2020-11-30 14:54:46 +0200

    Add files via upload

---
+++ Cikl s pred i post/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cikl_s_pred_i_post
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int a = int.Parse(chislo.Text);
+                int b = int.Parse(chislo2.Text);
+                int i = 0;
+                int j = 0;
+
+                while (i < 100) ;
+                {
+                j += i;
+                i += 4;
+                }
+                Result.Text = i + Environment.NewLine;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
